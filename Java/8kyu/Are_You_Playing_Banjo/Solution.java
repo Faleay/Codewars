f0a41c8789f73f6edf099b4c2709c142b69ac0fc
Alexey Boldizhar <99899710+Faleay@users.noreply.github.com>
2022-07-29 02:39:18 +0300

    Small edit in code

--- Java/8kyu/Are_You_Playing_Banjo/Solution.java
+++ Java/8kyu/Are_You_Playing_Banjo/Solution.java
@@ -2,4 +2,4 @@ class Banjo {
     public static String areYouPlayingBanjo(String name) {
       return name.toLowerCase().charAt(0) == 'r' ? name + " plays banjo" : name + " does not play banjo";
     }
-  }
+}
